6f35bc7bf4986b4de974c25f602c9d8a4986cabd
Matthew Wagerfield <matthew@wagerfield.com>
2026-06-17 17:02:31 +0100

    Add: reverse-a-linked-list - submission-4

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-4.ts
@@ -0,0 +1,31 @@
+// L1 -> L2
+// L2 -> L1
+
+// L1 -> L2 -> L3 -> L4
+// L4 -> L3 -> L2 -> L1
+
+interface Node {
+    val: number
+    next: Node | null
+}
+
+class Solution {
+    /**
+     * @param {Node} head
+     * @return {Node}
+     */
+    reverseList(head: Node | null): Node | null {
+        let curr: Node | null = head
+        let prev: Node | null = null
+        let next: Node | null = null
+
+        while (curr !== null) {
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        }
+
+        return prev
+    }
+}
