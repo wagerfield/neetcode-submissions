08f45444133ab73fbd28c3cf866287bb417fc3f0
Matthew Wagerfield <matthew@wagerfield.com>
2026-06-17 14:02:57 +0100

    Add: baseball-game - submission-6

---
+++ Data Structures & Algorithms/baseball-game/submission-6.ts
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {string[]} operations
+     * @return {number}
+     */
+    calPoints(operations: string[]): number {
+      let score = 0;
+      let total = 0;
+
+      const scores: number[] = [];
+
+      for (let i = 0, l = operations.length; i < l; i++) {
+        switch (operations[i]) {
+          case "+":
+            score = scores.at(-1) + scores.at(-2);
+            scores.push(score);
+            total += score;
+            continue;
+          case "C":
+            score = scores.pop();
+            console.log(score, total);
+            total -= score;
+            continue;
+          case "D":
+            score = scores.at(-1) * 2;
+            scores.push(score);
+            total += score;
+            continue;
+          default:
+            score = +operations[i];
+            scores.push(score);
+            total += score;
+        }
+      }
+
+      console.log(scores);
+
+      return total;
+    }
+}
