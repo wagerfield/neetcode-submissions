8315054214af3bd25cefb07b610093d632211279
Matthew Wagerfield <matthew@wagerfield.com>
2026-06-18 13:55:16 +0100

    Add: search-2d-matrix - submission-5

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-5.ts
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix: number[][], target: number): boolean {
+        const ROWS = matrix.length
+        const COLS = matrix[0].length
+        const SIZE = ROWS * COLS - 1
+
+        let l = 0
+        let r = SIZE
+
+        while (l <= r) {
+            const mid = l + Math.floor((r - l) / 2)
+            const row = Math.floor(mid / COLS)
+            const col = mid % COLS
+            const val = matrix[row][col]
+            
+            if (target > val) {
+                l = mid + 1
+            } else if (target < val) {
+                r = mid - 1
+            } else {
+                return true
+            }
+        }
+
+        return false
+    }
+}
