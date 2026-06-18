db3ddb46cb6632ff3518a775e6b01856aedffbfc
Matthew Wagerfield <matthew@wagerfield.com>
2026-06-18 08:59:50 +0100

    Add: implement-stack-using-queues - submission-2

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-2.ts
@@ -0,0 +1,43 @@
+class MyStack {
+    private queue: number[] = []
+
+    constructor() {}
+
+    /**
+     * @param {number} x
+     * @return {void}
+     */
+    push(x: number): void {
+        this.queue.push(x)
+    }
+
+    /**
+     * @return {number}
+     */
+    pop(): number {
+        return this.queue.pop()
+    }
+
+    /**
+     * @return {number}
+     */
+    top(): number {
+        return this.queue.at(-1)
+    }
+
+    /**
+     * @return {boolean}
+     */
+    empty(): boolean {
+        return this.queue.length === 0
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
