d3d148f912838ffaec111407ab1ed04adad0178f
Matthew Wagerfield <matthew@wagerfield.com>
2026-07-01 18:39:06 +0100

    Add: replace-elements-with-greatest-element-on-right-side - submission-6

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-6.ts
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @return {number[]}
+     */
+    replaceElements(arr: number[]): number[] {
+        let val, max = -1;
+        for (let i = arr.length - 1; i >= 0; i--) {
+            val = arr[i];
+            arr[i] = max;
+            max = val > max ? val : max;
+        }
+        return arr;
+    }
+}
