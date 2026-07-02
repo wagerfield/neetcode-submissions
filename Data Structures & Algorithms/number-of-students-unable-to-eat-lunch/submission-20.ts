7ac45259a8cbb736a2eca83073dbd30219d61c9c
Matthew Wagerfield <matthew@wagerfield.com>
2026-07-02 14:29:29 +0100

    Add: number-of-students-unable-to-eat-lunch - submission-20

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-20.ts
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {number[]} students
+     * @param {number[]} sandwiches
+     * @return {number}
+     */
+    countStudents(students: number[], sandwiches: number[]): number {
+        const prefs = new Uint32Array(2)
+        let unserved = students.length
+
+        for (const student of students) {
+            prefs[student]++
+        }
+
+        for (const sandwich of sandwiches) {
+            if (prefs[sandwich] > 0) {
+                prefs[sandwich]--
+                unserved--
+            } else break
+        }
+
+        return unserved
+    }
+}
+
+//   students=[1,1,1,0,0,1]
+// sandwiches=[1,0,0,0,1,1]
+//
+//   students=[1,1,0,0,1]
+// sandwiches=[0,0,0,1,1]
+//
+//   students=[1,0,0,1,1]
+// sandwiches=[0,0,0,1,1]
+//
+//   students=[0,0,1,1,1]
+// sandwiches=[0,0,0,1,1]
+//
+//   students=[0,1,1,1]
+// sandwiches=[0,0,1,1]
+//
+//   students=[1,1,1]
+// sandwiches=[0,1,1]
